5dc1e6ce8fd08e32a0a82c9b6cab8a9b1687edc7
ico4e <70281513+ico4e@users.noreply.github.com>
2020-09-22 13:21:36 -0400

    react-hook-form example v0.0.1

---
+++ src/components/UserSignUp.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {useForm} from 'react-hook-form';
+
+export default function UserSignUp() {
+  const {register, handleSubmit, errors} = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+    // e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Sign Up</h1>
+
+      <label>First Name</label>
+      <input name='firstName' ref={register({required: true, minLength: 2})} />
+      {errors.firstName && errors.firstName.type === 'required' && (
+        <p>First Name is required</p>
+      )}
+      {errors.firstName && errors.firstName.type === 'minLength' && (
+        <p>This field need minmium 2 charcaters</p>
+      )}
+
+      <label>Last Name</label>
+      <input type='text' name='lastName' ref={register({required: true})} />
+      {errors.lastName && errors.lastName.type === 'required' && (
+        <p>Last Name is required</p>
+      )}
+
+      <label>Gender</label>
+      <select name='gender' ref={register({required: true})}>
+        <option value=''>Select...</option>
+        <option value='female'>Female</option>
+        <option value='male'>Male</option>
+      </select>
+      {errors.gender && errors.gender.type === 'required' && (
+        <p>Gender is required</p>
+      )}
+
+      <label>Username</label>
+      <input type='text' name='username' ref={register({required: true})} />
+      {errors.username && errors.username.type === 'required' && (
+        <p>Username is required</p>
+      )}
+
+      <label>Email</label>
+      <input type='email' name='email' ref={register({required: true})} />
+      {errors.email && errors.email.type === 'required' && (
+        <p>Email is required</p>
+      )}
+
+      <label>About you</label>
+      <textarea
+        name='aboutYou'
+        ref={register()}
+        style={{resize: 'none'}}
+      ></textarea>
+
+      <input type='submit' />
+    </form>
+  );
+}
